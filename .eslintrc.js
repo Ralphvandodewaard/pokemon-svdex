f3400ca12020fbd410673922907c7255c86e9697
Ralphvandodewaard <rtvd@ziggo.nl>
2022-11-11 13:23:29 +0100

    Updated eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,18 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/component-name-in-template-casing': ['error', 'PascalCase', {
+      registeredComponentsOnly: false
+    }],
+    'vue/attribute-hyphenation': ['error', 'always', {
+      ignore: []
+    }],
+    semi: 'off',
+    '@typescript-eslint/semi': ['error', 'always'],
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': ['error'],
+    'space-before-function-paren': ['error', 'never'],
+    'spaced-comment': 'off'
   }
-}
+};
