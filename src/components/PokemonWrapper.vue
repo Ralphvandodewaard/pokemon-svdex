57aabdbcdef741b7b02cab50ef60f3ee181c6cd5
Ralphvandodewaard <rtvd@ziggo.nl>
2022-11-11 14:51:17 +0100

    Added pokemon sprite images

--- src/components/PokemonWrapper.vue
+++ src/components/PokemonWrapper.vue
@@ -1,11 +1,20 @@
 <template>
-  <div>
-    {{ pokemon.name }}
+  <div class="flex items-center gap-2">
+    <div class="flex justify-center items-center w-20 h-20 rounded-lg overflow-hidden">
+      <img
+        :alt="pokemon.name"
+        :src="getImage"
+        class="max-w-none"
+      >
+    </div>
+    <p>
+      {{ pokemon.name }}
+    </p>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from 'vue';
+import { defineComponent, PropType, computed } from 'vue';
 import Pokemon from '@/models/Pokemon';
 
 export default defineComponent({
@@ -15,6 +24,15 @@ export default defineComponent({
       type: Object as PropType<Pokemon>,
       required: true
     }
+  },
+  setup(props) {
+    const getImage = computed<string>(() => {
+      return require(`@/assets/sprites/${props.pokemon.name.toLowerCase()}.jpg`);
+    });
+
+    return {
+      getImage
+    };
   }
 });
 </script>
