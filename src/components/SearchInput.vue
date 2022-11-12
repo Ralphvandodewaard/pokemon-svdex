68f5d65bbb181a841863af0587eeb00f252d0b80
Ralphvandodewaard <rtvd@ziggo.nl>
2022-11-12 15:42:57 +0100

    Removed cross icon and functionality from searchinput

--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -15,25 +15,17 @@
       class="w-full pl-9 pr-8 py-1 placeholder-current bg-white rounded-lg outline-none"
       @input="$emit('update:modelValue', ($event.target as HTMLInputElement).value)"
     >
-    <label
-      for="search"
-      class="absolute top-0 right-0 flex items-center h-full pr-3"
-    >
-      <CrossIcon />
-    </label>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import SearchIcon from '@/assets/icons/search-icon.svg';
-import CrossIcon from '@/assets/icons/cross-icon.svg';
 
 export default defineComponent({
   name: 'SearchInput',
   components: {
-    SearchIcon,
-    CrossIcon
+    SearchIcon
   },
   props: {
     modelValue: {
