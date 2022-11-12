9fc025134fb0dbc1c01f7e0364036f47cf58f08a
Ralphvandodewaard <rtvd@ziggo.nl>
2022-11-12 19:01:24 +0100

    Fixed homeview width changing on no search results

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col items-center gap-4">
+  <div class="flex flex-col items-center gap-4 w-full">
     <SearchInput v-model="searchValue" />
     <div
       v-if="filteredPokemon.length > 0"
