f2487f05c9348b32562a48c885bd2746549a1002
Ralphvandodewaard <rtvd@ziggo.nl>
2022-11-16 14:50:42 +0100

    Added scrolltopbutton component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col items-center gap-4 h-screen p-4 sm:p-8 pb-8 sm:pb-16 bg-neutral-850 text-white font-sora overflow-y-scroll">
+  <div class="flex flex-col items-center gap-4 min-h-screen p-4 sm:p-8 pb-8 sm:pb-16 bg-neutral-850 text-white font-sora">
     <h1 class="text-2xl font-semibold">
       SV Dex
       <span class="text-xs font-normal">
@@ -7,5 +7,27 @@
       </span>
     </h1>
     <RouterView/>
+    <ScrollTopButton @click="scrollToTop" />
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import ScrollTopButton from './components/ScrollTopButton.vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    ScrollTopButton
+  },
+  setup() {
+    function scrollToTop(): void {
+      window.scrollTo(0, 0);
+    }
+
+    return {
+      scrollToTop
+    };
+  }
+});
+</script>
---
+++ src/components/ScrollTopButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <button
+    class="fixed bottom-2 right-2 sm:bottom-8 sm:right-8 flex justify-center items-center w-12 h-12 p-3 sm:w-16 sm:h-16 sm:p-4
+    bg-neutral-800 text-white border border-black rounded-full"
+  >
+    <AngleUpIcon class="h-full" />
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import AngleUpIcon from '@/assets/icons/angle-up-icon.svg';
+
+export default defineComponent({
+  name: 'ScrollTopButton',
+  components: {
+    AngleUpIcon
+  }
+});
+</script>
